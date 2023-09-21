443328dcc904a06989e7568ef9d69f1cf12ca59a
Chasty <coderessence@gmail.com>
2023-09-20 19:39:09 -0500

    feat: initial commit

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,16 +1,35 @@
-import '@tamagui/core/reset.css'
-import '@tamagui/polyfill-dev'
+import "@tamagui/core/reset.css";
+import "@tamagui/polyfill-dev";
 
-import { Button, TamaguiProvider, YStack } from 'tamagui'
+import { Button, TamaguiProvider, YStack } from "tamagui";
 
-import config from './tamagui.config'
+import config from "./tamagui.config";
+import { DialogDemo } from "./dialog-demo";
+import { ToastDemo } from "./toast-demo";
+import { ToastProvider, ToastViewport } from "@tamagui/toast";
 
 export const Root = () => {
   return (
     <TamaguiProvider config={config} defaultTheme="light">
-      <YStack f={1} ai="center" jc="center">
-        <Button>Hello world</Button>
-      </YStack>
+      <ToastProvider
+        swipeDirection="horizontal"
+        duration={6000}
+        native={
+          [
+            /* uncomment the next line to do native toasts on mobile. NOTE: it'll require you making a dev build and won't work with Expo Go */
+            //'mobile',
+          ]
+        }
+      >
+        <YStack f={1} ai="center" jc="center">
+          <Button>Hello world</Button>
+        </YStack>
+        Dialog Demo
+        <DialogDemo />
+        Toast Demo
+        <ToastDemo />
+        <ToastViewport />
+      </ToastProvider>
     </TamaguiProvider>
-  )
-}
+  );
+};
